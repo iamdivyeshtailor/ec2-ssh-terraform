4051756b2b8ebc4b7caac1517cb1938a109854a1
Divyesh Tailor <divyeshtailor012@gmail.com>
2024-02-19 21:27:47 +0530

    First commit

--- variables.tf
+++ variables.tf
@@ -1,21 +1,19 @@
 variable "ami" {
     type = string
-    default = "ami-03f4878755434977f"
+    default = "ami-valuef"
 }
 
 variable "key_pair_name" {
     type = string
-    default = "terraform_v2"
+    default = "key_pair_name_value"
 }
-
 
 variable "aws_access_key" {
     type = string
-    default = "AKIAXQ7MFOYLRHR4XAE6"
+    default = "aws_access_key_value"
 }
-
 
 variable "aws_secret_key" {
     type = string
-    default = "kp+/j5nVXJs9QdrGRZrL38fGpyduUkItOq6y1V+z"
+    default = "aws_secret_key_value"
 }
